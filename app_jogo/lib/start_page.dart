796110b078760e0219b942f056a65f938464030d
samuelsouzaleite <contato.samuelsl@gmail.com>
2024-06-19 16:51:01 -0300

    commit

--- app_jogo/lib/start_page.dart
+++ app_jogo/lib/start_page.dart
@@ -1,8 +1,49 @@
 import 'package:flutter/material.dart';
 import 'main.dart';
+import 'progress_manager.dart'; // Adicione a importação do gerenciador de progresso
 
 class StartPage extends StatelessWidget {
   const StartPage({Key? key}) : super(key: key);
+
+  void _resetProgress(BuildContext context) async {
+    await ProgressManager.saveProgress(1, 0);
+    await ProgressManager.saveProgress(2, 0);
+    await ProgressManager.saveProgress(3, 0);
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(
+        builder: (context) => const MyHomePage(title: 'Níveis'),
+      ),
+    );
+  }
+
+  void _showResetConfirmationDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Confirmação'),
+          content: const Text(
+              'Tem certeza de que deseja iniciar um novo jogo? Isso irá perder todo o seu progresso atual.'),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(); // Fechar o diálogo
+              },
+              child: const Text('Cancelar'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(); // Fechar o diálogo
+                _resetProgress(context);
+              },
+              child: const Text('Confirmar'),
+            ),
+          ],
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,15 +52,34 @@ class StartPage extends StatelessWidget {
         title: const Text('Jogo de Termo'),
       ),
       body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pushReplacement(
-              context,
-              MaterialPageRoute(
-                  builder: (context) => const MyHomePage(title: 'Níveis')),
-            );
-          },
-          child: const Text('Iniciar Novo Jogo'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              width: 200, // Defina uma largura fixa
+              child: ElevatedButton(
+                onPressed: () {
+                  _showResetConfirmationDialog(context);
+                },
+                child: const Text('Iniciar Novo Jogo'),
+              ),
+            ),
+            SizedBox(height: 16), // Espaço entre os botões
+            SizedBox(
+              width: 200, // Defina a mesma largura fixa
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) =>
+                            const MyHomePage(title: 'Níveis')),
+                  );
+                },
+                child: const Text('Continuar'),
+              ),
+            ),
+          ],
         ),
       ),
     );
